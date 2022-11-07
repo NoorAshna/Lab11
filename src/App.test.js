a221e88c5c721981ef0e1afe32839a0abc2ea9d6
Noor Ahmad <noorahmedspider@gmail.com>
2022-11-07 13:47:04 +0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,21 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("comp");
   expect(linkElement).toBeInTheDocument();
+  
+});
+test('renders learn react link', () => {
+  render(<App />);
+
+  const linkElement2 = screen.getByTestId("comp");
+  expect(linkElement2).toBeInTheDocument("Hello World");
+
+});
+test('renders learn react link', () => {
+  render(<App />);
+
+  const linkElement = screen.getByTestId("task3");
+  expect(linkElement).toBeInTheDocument("first item");
+
 });
